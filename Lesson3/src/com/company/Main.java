c9f5d2c6e1e29820658e16eee6e1a53267c60489
IlyaGolenya87 <golenya.info@gmail.com>
2020-05-31 14:07:01 +0300

    aaa

--- Lesson3/src/com/company/Main.java
+++ Lesson3/src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
         // массив - это именованая область памяти
         // в которой храниться несколько элементов
         // одного типа
+
+ /*
         int [] hello = new int[5];
         hello [0] = 12;
         hello [1] = 23;
@@ -16,84 +18,108 @@ public class Main {
         System.out.println(hello [2]);
         System.out.println(hello [3]);
 
+*/
+
+/*
+        // задания inline (в линию) массива
+
         float [] arr2 = {12.0f, 13.0f, 14.78f};
-        // задания inline массива
+
         // в значении свойства length
         // хранится кол-во элементов массива
+
         System.out.println(arr2.length);
-// многомерные массивы
+
+
+
+ */
+/*
+        // Многомерные массивы
+
         int [] [] arr3 = new int [3] [5];
+
         // 3 - кол-во строк
         // 5 - кол-во столбцов
+
         arr3 [1] [3] = 56;
-        // двумерный inline массив
-        float [] [] arr4 = {{1,2},{3,4},{5,6,8}};
+ */
+/*
+        // Двумерный inline массив
+
+        int  [] [] arr4 = {{1,2},{3,4},{5,6,8}};
+
         // 1.2
         //2.4
         //5,6,8
+
         arr4 [2] [2] = 9;
+
 
         // arr4 [0] [2] = 10; - ошибка
 
+        System.out.println(arr4);
+*/
 
         // ЦИКЛЫ
+
+/*
         int x = 0;
 
-        while (x<12) {
+        while (x < 12) {
             x = x + 1;
             System.out.println(x);
+
+
             // do....while - цикл с пост проверкой условия
         }
+
         do {
             System.out.println(x);
-        } while (x<5);
+        } while (x < 5);
 
         // break - операция прерывает выполнения циклы
         // continue - прерывает текущую итерацию цикла
 
-        int [] arr5 = {12,13,50,120,9};
+*/
+        int[] arr5 = {12, 13, 50, 120, 9};
         int index = 0;
-        while (index < arr5.length) {
-            if (arr5 [index]%9 == 0) {
-                System.out.println(arr5 [index]);
+/*        while (index < arr5.length) {
+            if (arr5[index] % 9 == 0) {
+                System.out.println(arr5[index]);
                 break;
             }
-            index= index + 1;
+            index = index + 1;
 
 
         }
-
-        index  = 0;
+*/
+/*        index = 0;
         while (index < arr5.length) {
-            if (arr5 [index]%9 != 0) {
-                index= index + 1;
+            if (arr5[index] % 2 != 0) {
+                index = index + 1;
                 continue;
             }
-            System.out.println(arr5 [index]);
-            index= index + 1;
+            System.out.println(arr5[index]);
+            index = index + 1;
         }
-
+*/
         // 1. блок инициализация
         // 2. блок условия цикла  выполнения код {}
         // 3. блок поствыполнения
         // любой из 3-ъх блок не обязателен
         // for (;;) {...} - бесконечный цикл
         // <2b-> code->3b->2b->=>b2 == true
-        for (int i = 0;i<arr5.length;i=i+1) {
-            System.out.println(arr5 [i]);
+
+/*        for (int i = 0; i < arr5.length; i = i + 1) {
+            System.out.println(arr5[i]);
         }
 
-        // foreach
-        for (int z: arr5) {
-            if (z%3==0) {
+*/        // foreach
+        for (int z : arr5) {
+            if (z % 3 == 0) {
                 System.out.println(z);
             }
         }
-
-        // +=, -=,/=,*=, %= - чтобы сократить форму записи
-        // ++, -- (унарные операторы, достаточно одно значение)
-
-
 
     }
 }
